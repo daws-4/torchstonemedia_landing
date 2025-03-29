c10c145eba94ea192bba858df50cb82fd4e9f920
toniborja40 <tonycoolpulido@gmail.com>
2025-03-29 17:41:54 -0400

    J Equipo2

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,24 @@
+"use client";
+
 import { title } from "@/components/primitives";
+import { User } from "@heroui/react" ; 
 
 export default function BlogPage() {
   return (
     <div>
+    <section>
       <h1 className={title()}>Nosotros</h1>
       <h1>coma toche</h1>
+    </section>     
+      
+      <User
+      avatarProps={{
+        src: "https://i.pravatar.cc/150?u=a04258114e29026702d",
+      }}
+
+      description="Product Designer"
+      name="Amanda Lee"
+      />
     </div>
   );
 }
