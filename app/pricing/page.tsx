d82794113b000437c20f3b394491ab80c1d1ed05
toniborja40 <tonycoolpulido@gmail.com>
2025-03-30 19:28:48 -0400

    J Equipo16

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,9 +1,20 @@
-import { title } from "@/components/primitives";
-
 export default function PricingPage() {
+
+  
   return (
-    <div>
-      <h1 className={title()}>Pricing</h1>
+  <div>
+  <div>
+    {/*Titulo*/}
+    <h1 className="text-5xl text-blue-950">Contacto</h1>
+  <br />
+    <h1 className="text-3xl">Nuestro equipo está a su disposición para responder todas sus inquietudes de manera eficiente y en el menor tiempo posible.</h1>
+    <br />
+  </div>
+  <div className="bg-white rounded-lg md:rounded-xl shadow-sm p-6 md:p-8 mb-8 md:mb-10 border border-gray-100 w-full">
+  <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 px-4">
+  
+      </div>
     </div>
+  </div>
   );
 }
