f87d6d3c0faf4a262c3bd6292410f92a8bc4bbd6
toniborja40 <tonycoolpulido@gmail.com>
2025-03-31 17:39:17 -0400

    J Equipo18

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -3,7 +3,7 @@ export default function AboutLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
+  return (   
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       
         {children}
